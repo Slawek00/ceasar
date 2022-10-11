d5e4de9bb4238efe2f80925932196160031f0610
Sławomir Jałojda <slawekjalojda@gmail.com>
2022-10-11 21:02:03 +0200

    Poprawki

--- ceasar_ cipher.js
+++ ceasar_ cipher.js
@@ -2,7 +2,7 @@ const cipherBreaker = (cipher, shift) => {
     const charsUpArray = ['A','B','C','D','E','F','G','H','I','J',
         'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     const charsLowArray = ['a','b','c','d','e','f','g','h','i','j',
-        'k','l','m','n','o','p','q','r','s','t','u','v','w','y','z'];
+        'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     const cipherSplit = cipher.split('');
     const tabOfIndex = [];
     const breakCipher = [];
@@ -29,8 +29,7 @@ const cipherBreaker = (cipher, shift) => {
             breakCipher.push(charsUpArray[tabOfIndex[i]]);
         }
 
-
-    } else if (cipher === cipher.toLowerCase()) {
+    } else {
 
         cipherSplit.forEach(element => {
             if(element === ' '){
@@ -39,8 +38,8 @@ const cipherBreaker = (cipher, shift) => {
             if( charsLowArray.includes(element) === true){
                 if(charsLowArray.indexOf(element)+shift < 26){
                     tabOfIndex.push(charsLowArray.indexOf(element)+shift);
-                }else {
-                    let div = 25-charsUpArray.indexOf(element);
+                }else{
+                    let div = 25-charsLowArray.indexOf(element);
                     let newShift = shift-div;
                     tabOfIndex.push(newShift-1);
                 }
@@ -69,10 +68,14 @@ const cipherBreaker = (cipher, shift) => {
 
 
 
-const ceasarCipher = 'XFO OXFA LK MBXOI EXOYLO U QEFP FP KLQ AOFII';
-const sentence = 'WP NSTQQCP OP GTRPYPCP PDE FY DJDEPXP OP NSTQQCPXPYE AZWJLWASLMPETBFP';
-const shift = 3;
-const shift2 = 15;
+let cipherUp = 'XFO OXFA LK MBXOI EXOYLO U QEFP FP KLQ AOFII';
+let cipherLow = cipherUp.toLowerCase();
+let sentence = 'WP NSTQQCP OP GTRPYPCP PDE FY DJDEPXP OP NSTQQCPXPYE AZWJLWASLMPETBFP';
+let sentenceLow = sentence.toLowerCase();
+let shift = 3;
+let shift2 = 15;
 
-console.log(cipherBreaker(ceasarCipher,shift));
+console.log(cipherBreaker(cipherUp,shift));
 console.log(cipherBreaker(sentence, shift2));
+console.log(cipherBreaker(cipherLow,shift));
+console.log(cipherBreaker(sentenceLow,shift2));
